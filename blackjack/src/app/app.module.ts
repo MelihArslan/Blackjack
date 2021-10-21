2badd07a78d767fe7120e804cfd092364d5a82b7
burgerl1 <leander.burger@hva.nl>
2021-10-21 20:05:40 +0200

    Sidebar toegevoegd.

--- blackjack/src/app/app.module.ts
+++ blackjack/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { HeaderComponent } from './header/header.component';
 import { TestComponent } from './header/test/test.component';
 import { FooterComponent } from './footer/footer.component';
 import { NavbarComponent } from './header/navbar/navbar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
@@ -13,7 +14,8 @@ import { NavbarComponent } from './header/navbar/navbar.component';
     HeaderComponent,
     TestComponent,
     FooterComponent,
-    NavbarComponent
+    NavbarComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule
