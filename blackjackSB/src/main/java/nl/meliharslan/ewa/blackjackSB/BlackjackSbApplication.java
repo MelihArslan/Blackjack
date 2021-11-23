f71899c4cbea935a79a2dccb526ded8f08846c74
arslanm4 <Rv9HwptsM3jp@>
2021-11-23 15:38:18 +0100

    Made a new Spring Boot application with proper name and reformatted name of Angular application. Angular worked on port 4200 and Spring Boot worked on port 8080 (On the same time).

---
+++ blackjackSB/src/main/java/nl/meliharslan/ewa/blackjackSB/BlackjackSbApplication.java
@@ -0,0 +1,13 @@
+package nl.meliharslan.ewa.blackjackSB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlackjackSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlackjackSbApplication.class, args);
+	}
+
+}
---
+++ blackjackSB/src/test/java/nl/meliharslan/ewa/blackjackSB/BlackjackSbApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.meliharslan.ewa.blackjackSB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlackjackSbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
