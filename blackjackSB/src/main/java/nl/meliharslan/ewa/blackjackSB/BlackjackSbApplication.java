b5523343b586f40ea66b5d8d9bfe13620a80f215
arslanm4 <Rv9HwptsM3jp@>
2021-11-25 10:06:24 +0100

    Added TestController with test endpoint

--- blackjackSB/src/main/java/nl/meliharslan/ewa/blackjackSB/BlackjackSbApplication.java
+++ blackjackSB/src/main/java/nl/meliharslan/ewa/blackjackSB/BlackjackSbApplication.java
@@ -2,6 +2,10 @@ package nl.meliharslan.ewa.blackjackSB;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @SpringBootApplication
 public class BlackjackSbApplication {
