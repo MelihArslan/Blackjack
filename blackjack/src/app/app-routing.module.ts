8a48a2306175c4a3e4b6c4b2f57972a29e2853ca
burgerl1 <leander.burger@hva.nl>
2021-11-25 11:31:52 +0100

    frontend gametable achter join game aangemaakt

--- blackjack/src/app/app-routing.module.ts
+++ blackjack/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import {RouterModule, Routes} from "@angular/router";
 import {BodyComponent} from "./body/body.component";
 import {LoginComponent} from "./login/login.component";
 import {RegisterComponent} from "./register/register.component";
+import {TableComponent} from "./table/table.component";
 
 const routes: Routes = [
   {path: '', redirectTo: '/home', pathMatch: 'full'},
   {path: 'home', component: BodyComponent},
   {path: 'login', component: LoginComponent},
-  {path: 'register', component: RegisterComponent}
+  {path: 'register', component: RegisterComponent},
+  {path: 'joingame', component: TableComponent}
 ]
 
 @NgModule({
--- blackjack/src/app/app.module.ts
+++ blackjack/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import {FormsModule} from "@angular/forms";
 import {UserService} from "./services/user.service";
+import { TableComponent } from './table/table.component';
 
 @NgModule({
   declarations: [
@@ -27,7 +28,8 @@ import {UserService} from "./services/user.service";
     IconGridComponent,
     CarouselComponent,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    TableComponent
   ],
     imports: [
         BrowserModule,
